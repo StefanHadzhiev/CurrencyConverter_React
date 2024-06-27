e9b5802bc2ff37ff142a1142b8dc88f282f43139
StefanHadzhiev <stefanhadzhiev24@gmail.com>
2024-06-27 09:49:11 +0300

    Implementation

--- src/App.js
+++ src/App.js
@@ -1,22 +1,87 @@
 // `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
+import { useEffect, useState } from "react";
+
+const tempCurrencies = ["USD", "EUR", "CAD", "INR"];
+
 export default function App() {
+  const [amount, setAmount] = useState(0);
+  const [currencyFrom, setCurrencyFrom] = useState("EUR");
+  const [currencyTo, setCurrencyTo] = useState("USD");
+  const [output, setOutput] = useState(0);
+
+  useEffect(
+    function () {
+      async function fetchValueExchange() {
+        try {
+          const res = await fetch(
+            `https://api.frankfurter.app/latest?amount=${amount}&from=${currencyFrom}&to=${currencyTo}`
+          );
+
+          if (!res.ok) {
+            throw new Error("Something went wrong with fetching movies.");
+          }
+
+          const data = await res.json();
+          console.log(data.rates[currencyTo]);
+          setOutput(data.rates[currencyTo]);
+        } catch (err) {
+          console.log(err.message);
+        } finally {
+        }
+      }
+
+      fetchValueExchange();
+    },
+    [currencyFrom, currencyTo, amount]
+  );
+
+  function handleSetAmount(amount) {
+    setAmount(amount);
+  }
+
+  function handleCurrencyFrom(currency) {
+    setCurrencyFrom(currency);
+  }
+
+  function handleCurrencyTo(currency) {
+    setCurrencyTo(currency);
+  }
+
   return (
     <div>
-      <input type="text" />
-      <select>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
-      </select>
-      <select>
-        <option value="USD">USD</option>
-        <option value="EUR">EUR</option>
-        <option value="CAD">CAD</option>
-        <option value="INR">INR</option>
-      </select>
-      <p>OUTPUT</p>
+      <AmountInput amount={amount} setAmount={handleSetAmount} />
+      <CurrencySelector onChange={handleCurrencyFrom}>
+        {currencyFrom}
+      </CurrencySelector>
+      <CurrencySelector onChange={handleCurrencyTo}>
+        {currencyTo}
+      </CurrencySelector>
+      <Output output={output} />
     </div>
   );
+}
+
+function AmountInput({ amount, setAmount }) {
+  return (
+    <input
+      type="text"
+      value={amount === 0 ? "" : amount}
+      onChange={(e) => setAmount(e.target.value)}
+    />
+  );
+}
+
+function CurrencySelector({ onChange, children }) {
+  return (
+    <select onChange={(e) => onChange(e.target.value)} value={children}>
+      {tempCurrencies.map((curr) => (
+        <option key={curr}>{curr}</option>
+      ))}
+    </select>
+  );
+}
+
+function Output({ output }) {
+  return <p>{output}</p>;
 }
